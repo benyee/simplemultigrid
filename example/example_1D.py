89114e474987020ca474246c54a41dca529233db
Ben C. Yee <bcyee@umich.edu>
2017-10-12 22:33:54 -0400

    Print out final error instead of final solution in Ex.1
    Also minor changes to problem definition.

--- example/example_1D.py
+++ example/example_1D.py
@@ -6,13 +6,13 @@ sys.path.insert(0, '..')
 from py_simplemg import MultigridOptions, SmootherOptions, solve_multigrid
 import numpy as np
 
-nx = 2**10+1
+nx = 2**9+1
 A = np.zeros((nx, nx))
 b = np.zeros(nx)
 for i in range(nx):
   if i > 0:
     A[i, i-1] = -1
-  A[i, i] = 2.0001
+  A[i, i] = 2.01
   if i < nx-1:
     A[i, i+1] = -1
 
@@ -30,4 +30,6 @@ my_smooth_opts = SmootherOptions(smoothdown=1,
                                  num_color=2,
                                  color_flip=False,
                                  sparse=True)
-print(solve_multigrid(A, b, x, my_mg_opts, my_smooth_opts))
+x = solve_multigrid(A, b, x, my_mg_opts, my_smooth_opts)
+print("Final L2 error = ", np.linalg.norm(x,2)/np.sqrt(len(x)))
+print("Final Linf error = ", max(x))
